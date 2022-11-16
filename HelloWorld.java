902201c794255169c5b8d3bee82d788bc035a967
tnalss <indoubt@naver.com>
2022-11-16 13:09:49 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
-<<<<<<< HEAD
-		System.out.println("Merge Fuck!");
-	}//main()
-}//class
-=======
+
 		System.out.println("Hello World!!!");
 		System.out.println("Hello World!!!");
 		System.out.println("Hello World!!!");
 	}// main()
 }// class
->>>>>>> sumin
